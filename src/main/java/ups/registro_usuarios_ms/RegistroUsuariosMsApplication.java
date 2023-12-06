b7a4809135fa349f5d60fa87e29b3a2e7ec866e5
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-12-05 23:37:25 -0500

    commit inicial

---
+++ src/main/java/ups/registro_usuarios_ms/RegistroUsuariosMsApplication.java
@@ -0,0 +1,13 @@
+package ups.registro_usuarios_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroUsuariosMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroUsuariosMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ups/registro_usuarios_ms/RegistroUsuariosMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ups.registro_usuarios_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroUsuariosMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

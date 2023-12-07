1ebe0c885ad679e345dd86a79d82e91215f8e8b4
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-12-06 22:30:32 -0500

    agregado manejo de expeciones

---
+++ src/main/java/ups/registro_usuarios_ms/exeptions/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package ups.registro_usuarios_ms.exeptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
